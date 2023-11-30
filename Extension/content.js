dc57af641fa080839907cc2aedcd703e2df535c9
SubhanWasif <rustzee93@gmail.com>
2023-11-30 19:07:43 +0500

    sidebarFunction

--- Extension/content.js
+++ Extension/content.js
@@ -1,56 +1,57 @@
 // content.js
 
 // Function to send a message to the background script
-// function sendMessageToBackground(message) {
-//     return new Promise((resolve, reject) => {
-//       chrome.runtime.sendMessage(message, (response) => {
-//         if (chrome.runtime.lastError) {
-//           reject(chrome.runtime.lastError);
-//         } else {
-//           resolve(response);
-//         }
-//       });
-//     });
-//   }
+function sendMessageToBackground(message) {
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage(message, (response) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
   
-//   // Function to capture the current video frame
-//   async function captureFrame() {
-//     const video = document.querySelector('video');
-//     if (video) {
-//       // Pause the video
-//       video.pause();
+  // Function to capture the current video frame
+  async function captureFrame() {
+    const video = document.querySelector('video');
+    console.log(video)
+    if (video) {
+      // Pause the video
+      video.pause();
   
-//       // Create a canvas to capture the current frame
-//       const canvas = document.createElement('canvas');
-//       canvas.width = video.videoWidth;
-//       canvas.height = video.videoHeight;
-//       const ctx = canvas.getContext('2d');
-//       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      // Create a canvas to capture the current frame
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
   
-//       // Convert the frame to a data URL
-//       const frameDataURL = canvas.toDataURL('image/png');
+      // Convert the frame to a data URL
+      const frameDataURL = canvas.toDataURL('image/png');
   
-//       // Send the frame to the background script
-//       const response = await sendMessageToBackground({
-//         action: "processFrame",
-//         dataURL: frameDataURL
-//       });
+      // Send the frame to the background script
+      const response = await sendMessageToBackground({
+        action: "processFrame",
+        dataURL: frameDataURL
+      });
   
-//       // Process the annotations received from the background script
-//       if (response.annotations) {
-//         // TODO: Overlay annotations on the video
-//         // console.log(response.annotations);
-//       }
-//     }
-//   }
+      // Process the annotations received from the background script
+      if (response.annotations) {
+        // TODO: Overlay annotations on the video
+        // console.log(response.annotations);
+      }
+    }
+  }
   
-//   // Listen for a message from the popup or background script
-//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.action === "togglePause") {
-//       captureFrame().then(sendResponse).catch(console.error);
-//       return true; // indicates you wish to send a response asynchronously
-//     }
-//   });
+  // Listen for a message from the popup or background script
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "togglePause") {
+      captureFrame().then(sendResponse).catch(console.error);
+      return true; // indicates you wish to send a response asynchronously
+    }
+  });
   // Function to insert the sidebar HTML into the page
 function insertSidebar() {
     const sidebarHTML = `
@@ -59,6 +60,8 @@ function insertSidebar() {
         <a href="#" id="settings-icon" class="sidebar-icon">⚙️</a>
         <a href="#" id="info-icon" class="sidebar-icon">❓</a>
       </div>
+      <div id="player"></div>
+      <script src="https://www.youtube.com/iframe_api"></script>
     `;
     document.body.insertAdjacentHTML('beforeend', sidebarHTML);
   }
@@ -110,28 +113,25 @@ function insertSidebar() {
     }
   }
 
+  function toggleSidebar(displayState) {
+    const sidebar = document.getElementById('my-extension-sidebar');
+    if (sidebar) {
+      sidebar.style.display = displayState;
+    }
+  }
   
   // Function to handle video play and pause events
   function handleVideoPlayback() {
-    var video = document.querySelector('video');
+    const video = document.querySelector('video');
     console.log(video);
-    if (video[0].paused)
-   {
-      console.log("video is stopped");
-      //DO SOMETING...
-  } else {
-      console.log("video is playing");
-      //DO SOMETHING...
+    if (video) {
+      video.addEventListener('pause', () => toggleSidebar('flex'));
+      video.addEventListener('play', () => toggleSidebar('none'));
+    }
   }
-
-  }
-
-
-
-
   
   // Initially hide the sidebar
-  toggleSidebar('flex');
+  toggleSidebar('none');
   
   // Start handling video playback events
-  (handleVideoPlayback());
+  handleVideoPlayback();
